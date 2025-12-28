4ea6356b516a37ceb03a44d5a53d73592c2639b6
CarbonStudios <astrascripts7@gmail.com>
2025-12-28 18:51:39 -0500

    Update CSLoader.lua

--- CSLoader.lua
+++ CSLoader.lua
@@ -12,19 +12,19 @@ elseif gameId == 76558904092080 then -- forge world 1
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/forge.lua"))()
 
 elseif gameId == 129009554587176 then -- forge world 2
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/forge.lua"))()
+    loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/forge.lua"))()
 
 elseif gameId ==  then -- forge world 3
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/forge.lua"))()
+    loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/forge.lua"))()
 
 elseif gameId == 2753915549 then -- bloxfruits sea 1
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/bloxfruits.lua"))()
+    loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/bloxfruits.lua"))()
     
 elseif gameId == 79091703265657 then -- bloxfruits sea 2
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/bloxfruits.lua"))()
+    loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/bloxfruits.lua"))()
 
 elseif gameId == 100117331123089 then -- bloxfruits sea 3
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/bloxfruits.lua"))()
+    loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/bloxfruits.lua"))()
 
 else
     game.StarterGui:SetCore("SendNotification", {
