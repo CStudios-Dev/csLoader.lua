0aa1c3390725e2d32126b941573090b59a19321f
CarbonStudios <astrascripts7@gmail.com>
2025-12-28 18:58:41 -0500

    Update CSLoader.lua

--- CSLoader.lua
+++ CSLoader.lua
@@ -10,22 +10,19 @@ elseif gameId == 79546208627805 then -- 99nights
     
 elseif gameId == 76558904092080 then -- forge world 1
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CStudios-Dev/loader.lua/main/forge.lua"))()
-
+    
 elseif gameId == 129009554587176 then -- forge world 2
     loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/forge.lua"))()
-
-elseif gameId ==  then -- forge world 3
-    loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/forge.lua"))()
-
+    
 elseif gameId == 2753915549 then -- bloxfruits sea 1
     loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/bloxfruits.lua"))()
     
-elseif gameId == 79091703265657 then -- bloxfruits sea 2
+elseif gameId == 4442272183 then -- bloxfruits sea 2
     loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/bloxfruits.lua"))()
-
-elseif gameId == 100117331123089 then -- bloxfruits sea 3
+    
+elseif gameId == 7449423635 then -- bloxfruits sea 3
     loadstring(game:HttpGet("https://github.com/CStudios-Dev/csLoader.lua/main/bloxfruits.lua"))()
-
+    
 else
     game.StarterGui:SetCore("SendNotification", {
         Title = "Not Supported";
